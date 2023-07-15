ffc414d518cc57416774e751793082f94962bbb6
박윤찬 <dbscks9793@gmail.com>
2023-07-15 16:24:55 +0900

    Initial Commit

---
+++ src/test/java/com/devprofile/DevProfile/DevProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devprofile.DevProfile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevProfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
