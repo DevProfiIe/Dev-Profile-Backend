11bc77b25bb598dc23b471fdca46f23f2dc6be7f
박윤찬 <dbscks9793@gmail.com>
2023-07-16 16:17:07 +0900

    Feat:사용자 정보 db 저장

--- src/main/java/com/devprofile/DevProfile/controller/LoginController.java
+++ src/main/java/com/devprofile/DevProfile/controller/LoginController.java
@@ -1,17 +1,28 @@
 package com.devprofile.DevProfile.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+
 
 @Controller
+@RequiredArgsConstructor
 public class LoginController {
+
+    @GetMapping("/")
+    public String login(Model model, @AuthenticationPrincipal OAuth2User oauth2User) {
+        model.addAttribute("username", oauth2User.getAttribute("name"));
+        return "index";
+    }
+
     @GetMapping("/CustomLogin")
-    public String customLogin(Model model, @AuthenticationPrincipal OAuth2User oauth2User) {
-        String userName = oauth2User.getAttribute("login");
-        model.addAttribute("userName", userName);
+    public String customLogin(Model model, @AuthenticationPrincipal OAuth2User oauth2User){
+        model.addAttribute("username", oauth2User.getAttribute("name"));
         return "CustomLogin";
     }
+
+
 }
