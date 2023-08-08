012eec01dfe92a5706a609ede5ff60eb4b78f5d6
박윤찬 <dbscks9793@gmail.com>
2023-08-08 22:44:33 +0900

    Fix: Patch 성능 개선

--- src/main/java/com/devprofile/DevProfile/repository/PatchRepository.java
+++ src/main/java/com/devprofile/DevProfile/repository/PatchRepository.java
@@ -14,8 +14,9 @@ public interface PatchRepository extends JpaRepository<PatchEntity,Long> {
     List<String> findExistingOids(@Param("oids") List<String> oids);
 
     List<PatchEntity> findByCommitOid(String commitOid);
-    List<PatchEntity> findByPatch(String patch);
+
 
     List<PatchEntity> findAllByPatch(String patch);
+    List<PatchEntity> findAllByPatchIn(List<String> patches);
 
 }
--- src/main/java/com/devprofile/DevProfile/service/rabbitmq/MessageSenderService.java
+++ src/main/java/com/devprofile/DevProfile/service/rabbitmq/MessageSenderService.java
@@ -4,7 +4,9 @@ import com.devprofile.DevProfile.entity.CommitEntity;
 import com.devprofile.DevProfile.entity.PatchEntity;
 import com.devprofile.DevProfile.entity.RepositoryEntity;
 import com.devprofile.DevProfile.entity.UserEntity;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -12,6 +14,7 @@ import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 @Service
+@Slf4j
 public class MessageSenderService {
 
     @Autowired
@@ -49,11 +52,15 @@ public class MessageSenderService {
         });
     }
 
-    public Mono<String> PatchSendMessage(PatchEntity patchEntity) {
-        return Mono.fromCallable(() -> {
-            String message = objectMapper.writeValueAsString(patchEntity);
-            amqpTemplate.convertAndSend(exchange, routingkey, message);
-            return "Message sent for Patch: " + patchEntity.getCommitOid();
+    public Mono<Void> PatchSendMessage(PatchEntity patchEntity) {
+        return Mono.fromRunnable(() -> {
+            try {
+                String message = objectMapper.writeValueAsString(patchEntity);
+                amqpTemplate.convertAndSend(exchange, routingkey, message);
+                log.info("Message sent for Patch: " + patchEntity.getCommitOid());
+            } catch (JsonProcessingException e) {
+                log.error("Error serializing PatchEntity: ", e);
+            }
         });
     }
 
